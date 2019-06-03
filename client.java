bc13c82346b41a7620278d7159aaf8d90d7a7727
Yu-HsuanLiu <48612762+Yu-HsuanLiu@users.noreply.github.com>
2019-06-03 15:04:45 +0800

    Update client.java

--- client.java
+++ client.java
@@ -1,31 +1,28 @@
-// client.java
 import java.net.*;
 import java.io.*;
-
-public class client
-{
-   public static void main(String [] args)
-   {
-      String serverName = args[0];
-      int port = Integer.parseInt(args[1]);
-      try
-      {
-         System.out.println("Connecting to " + serverName + " on port " + port);
-         Socket client = new Socket(serverName, port);
-         System.out.println("Just connected to " + client.getRemoteSocketAddress());
-         OutputStream outToServer = client.getOutputStream();
-         DataOutputStream out =
-                       new DataOutputStream(outToServer);
-         UserDomain = java.net.InetAddress.getLocalHost();
-         out.writeUTF("Hello from " + UserDomain.getHostName());
-         InputStream inFromServer = client.getInputStream();
-         DataInputStream in =
-                        new DataInputStream(inFromServer);
-         System.out.println("Server says " + in.readUTF());
-         client.close();
-      }catch(IOException e)
-      {
-         e.printStackTrace();
-      }
-   }
+public class client {
+    public static void main(String [] args)
+    {
+        String serverName = args[0];
+        int port = Integer.parseInt(args[1]);
+        try
+        {
+            System.out.println("Connecting to " + serverName + " on port " + port);
+            Socket client = new Socket(serverName, port);
+            System.out.println("Just connected to " + client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out =
+                    new DataOutputStream(outToServer);
+            InetAddress addr = InetAddress.getLocalHost();
+            out.writeUTF("Hello from " + addr.getHostName());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in =
+                    new DataInputStream(inFromServer);
+            System.out.println("Server says " + in.readUTF());
+            client.close();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
